e195c19e9305c0b63c06d290b815cb24a0cd6a72
dngxue <paolareyes802@gmail.com>
2025-06-09 18:12:54 -0600

    primera version navbar terminado

--- medical-prescriptions-crypto-client/src/pages/Login.jsx
+++ medical-prescriptions-crypto-client/src/pages/Login.jsx
@@ -1,10 +1,12 @@
 import { ThemeProvider } from '@mui/material/styles';
 import { Container, Stack, Box, Typography } from '@mui/material';
 import ThemeMaterialUI from '../components/ThemeMaterialUI.js';
+import Navbar from '../components/Navbar.jsx';
 
 const Login = () => {
     return (
         <ThemeProvider theme={ThemeMaterialUI}>
+            <Navbar/>
             <p>OALSA</p>
             <Typography variant='body2'>hola a todos</Typography>
         </ThemeProvider>
