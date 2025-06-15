1dd682cf4438fb8771578b878831f1a5236a0140
dngxue <paolareyes802@gmail.com>
2025-06-14 19:49:58 -0600

    registro farmaceutico

--- medical-prescriptions-crypto-client/src/components/admin/RegisterUser/RegisterPharmacist.jsx
+++ medical-prescriptions-crypto-client/src/components/admin/RegisterUser/RegisterPharmacist.jsx
@@ -1,56 +1,78 @@
-import { Box, Card, TextField, CardContent } from '@mui/material';
+import { useState } from 'react';
+import { Box, Card, CardContent } from '@mui/material';
 import Subtitle from '../../layout/Subtitle';
 import ButtonsMod from '../../layout/ButtonsMod';
 import RegisterHomeButton from './RegisterHomeButton';
+import RegisterTextField from './RegisterTextField';
 
 function RegisterPharmacist ({ setView }) {
+  const [formData, setFormData] = useState({
+      email: '',
+      nombre: '',
+      fechaNacimiento: '',
+      farmacia: '',
+      tel: '',
+      password: '',
+    });
+
+    const handleChange = (e) => {
+      const { name, value } = e.target;
+      setFormData((prev) => ({ ...prev, [name]: value }));
+    };
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      console.log('Datos: ', formData);
+    };
+
   return (
     <Box width='100%' sx={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center' }}>
       <RegisterHomeButton setView={setView} />
-      <Card sx={{ width:'80%', padding: '3%' }}>
+      <Card sx={{ width: {xs: '80%', md: '50%' }, padding: '3%' }}>
         <CardContent>
           <Subtitle subtitulo='Registrar farmacéutico' />
-          <TextField
-            sx={{ width: '100%', marginBottom: '20px' }}
-            id='filled-multiline-static'
+          {/* Correo electrónico ------------ */}
+          <RegisterTextField 
+            campo='email'
+            formData={formData} 
+            handleChange={handleChange}
             label='Correo electrónico'
-            multiline
-            rows={1}
           />
-          <TextField
-            sx={{ width: '100%', marginBottom: '20px' }}
-            id='filled-multiline-static'
+          {/* Nombre ------------------------ */}
+          <RegisterTextField 
+            campo='nombre'
+            formData={formData}
+            handleChange={handleChange}
             label='Nombre'
-            multiline
-            rows={1}
           />
-          <TextField
-            sx={{ width: '100%', marginBottom: '20px' }}
-            id='filled-multiline-static'
-            label='Fecha de nacimiento'
-            multiline
-            rows={1}
+          {/* Fecha de nacimiento ------------ */}
+          <RegisterTextField 
+            campo='fechaNacimiento'
+            formData={formData}
+            handleChange={handleChange}
+            label='Fecha de Nacimiento'
+            type='date'
           />
-          <TextField
-            sx={{ width: '100%', marginBottom: '20px' }}
-            id='filled-multiline-static'
+          {/* Farmacia ----------------------- */}
+          <RegisterTextField 
+            campo='farmacia'
+            formData={formData} 
+            handleChange={handleChange}
             label='Farmacia'
-            multiline
-            rows={1}
           />
-          <TextField
-            sx={{ width: '100%', marginBottom: '20px' }}
-            id='filled-multiline-static'
+          {/* Teléfono ----------------------- */}
+          <RegisterTextField 
+            campo='tel'
+            formData={formData}
+            handleChange={handleChange}
             label='Teléfono celular'
-            multiline
-            rows={1}
           />
-          <TextField
-            sx={{ width: '100%', marginBottom: '20px' }}
-            id='filled-multiline-static'
+          {/* Contraseña --------------------- */}
+          <RegisterTextField 
+            campo='password'
+            formData={formData}
+            handleChange={handleChange}
             label='Contraseña'
-            multiline
-            rows={1}
           />
 
           <Box sx={{ display: 'flex', width: '100%', marginTop: '10px' }}>
@@ -58,7 +80,7 @@ function RegisterPharmacist ({ setView }) {
               variant='principal'
               textCont='Registrar'
               width='100%'
-              clickEvent=''
+              clickEvent={handleSubmit}
               type='submit'
             />
           </Box>
