cadac6ff86b79f1b96ed23b9ef2c1aec559a8d60
dngxue <paolareyes802@gmail.com>
2025-06-23 21:52:23 -0600

    Cambios en el médico

--- medical-prescriptions-crypto-client/src/components/medic/M_Search/M_SearchContent/M_SCInformation.jsx
+++ medical-prescriptions-crypto-client/src/components/medic/M_Search/M_SearchContent/M_SCInformation.jsx
@@ -3,11 +3,11 @@ import { Box, Stack, Typography }  from '@mui/material';
 function M_SCInformation ({ paciente }) {
   return (
     <Box sx={{ display: 'flex', justifyContent: 'space-between', width: '100%', marginBottom: '50px' }}>  
-      <Stack direction='column' sx={{ width: {md: '60%', xs:'100%'}, marginBottom: '30px' }}>
+      <Stack direction='column' sx={{ width: {md: '60%', xs:'100%'}}}>
         <Typography variant='body1' fontWeight='bold' color='#4224B0'>
           Matrícula: {paciente.matricula}
         </Typography>
-        <Typography variant='body1' fontWeight={'bold'}>
+        <Typography>
           CURP: {paciente.curp}
         </Typography>
         <Typography>
